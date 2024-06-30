d8e883800adb945deda0f373b8902479a833c6c3
Vibhor Kumar Vatsa <vkumarvatsa@gmail.com>
2024-06-30 12:11:51 +0530

    Learning about Component generation

--- crew-management/src/app/app.component.ts
+++ crew-management/src/app/app.component.ts
@@ -1,12 +1,14 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import {MatButtonModule} from '@angular/material/button';
+import { CrewMembersComponent } from "./myComponents/crew-members/crew-members.component";
 
 @Component({
-  selector: 'app-root',
-  standalone: true,
-  imports: [RouterOutlet],
-  templateUrl: './app.component.html',
-  styleUrl: './app.component.css'
+    selector: 'app-root',
+    standalone: true,
+    templateUrl: './app.component.html',
+    styleUrl: './app.component.css',
+    imports: [RouterOutlet, MatButtonModule, CrewMembersComponent]
 })
 export class AppComponent {
   title = 'crew-management';
