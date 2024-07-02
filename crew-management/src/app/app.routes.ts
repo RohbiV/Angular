9735517e9755c2c82c5e632e62aaef1717316c89
Vibhor Kumar Vatsa <vkumarvatsa@gmail.com>
2024-07-02 22:47:07 +0530

    added lazy loading for add-user

--- crew-management/src/app/app.routes.ts
+++ crew-management/src/app/app.routes.ts
@@ -4,6 +4,10 @@ export const routes: Routes = [
     {
         path: 'admin',
         loadChildren: () => import('./admin/admin.module').then(m => m.AdminModule)
+    },
+    {
+        path: 'add-user',
+        loadComponent: () => import('./admin/add-user/add-user.component').then(m=>m.AddUserComponent)
     }
 ];
 
