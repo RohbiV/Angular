3500c2c3b9d29cd4bfa4bf3b3a8990877bef9fd4
Vibhor Kumar Vatsa <vkumarvatsa@gmail.com>
2024-07-04 17:37:41 +0530

    Added directive in appcomponent

--- crew-management/src/app/app.component.ts
+++ crew-management/src/app/app.component.ts
@@ -3,13 +3,14 @@ import { RouterOutlet } from '@angular/router';
 import {MatButtonModule} from '@angular/material/button';
 import { CrewMembersComponent } from "./myComponents/crew-members/crew-members.component";
 import { UsersModule } from './users/users.module';
+import { HighlightDirective } from './highlight.directive';
 
 @Component({
     selector: 'app-root',
     standalone: true,
     templateUrl: './app.component.html',
     styleUrl: './app.component.css',
-    imports: [RouterOutlet, MatButtonModule, CrewMembersComponent,UsersModule]
+    imports: [RouterOutlet, MatButtonModule, CrewMembersComponent,UsersModule,HighlightDirective]
 })
 export class AppComponent {
   title = 'crew-management';
